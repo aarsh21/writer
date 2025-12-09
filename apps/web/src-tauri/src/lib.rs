c5d18c3eb0ce2b7d8e28d447dfa8fc7bed88d5ef
aarsh21 <aarshpersonal@proton.me>
2025-12-09 17:58:27 +0530

    DEV: remove decorations from the prod build and add debug assertion for zoom

--- apps/web/src-tauri/src/lib.rs
+++ apps/web/src-tauri/src/lib.rs
@@ -1,27 +1,24 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-  // TODO: test for other linux distros
-    .on_page_load(|window, _payload| {
-      let zoom_level = if cfg!(target_os = "linux") {
-        1.4
-      } else {
-        1.0
-      };
-      if let Err(e) = window.set_zoom(zoom_level) {
-        eprintln!("Failed to set zoom: {}", e);
-      }
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        .on_page_load(|window, _payload| {
+            if cfg!(debug_assertions) {
+                let zoom_level = if cfg!(target_os = "linux") { 1.3 } else { 1.0 };
+                if let Err(e) = window.set_zoom(zoom_level) {
+                    eprintln!("Failed to set zoom: {}", e);
+                }
+            }
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
