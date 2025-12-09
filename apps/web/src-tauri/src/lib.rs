f47906c706873100a85b93b362104a127b565079
aarsh21 <aarshpersonal@proton.me>
2025-12-09 11:29:33 +0530

    fix: scaling for linux/my setup

--- apps/web/src-tauri/src/lib.rs
+++ apps/web/src-tauri/src/lib.rs
@@ -11,6 +11,17 @@ pub fn run() {
       }
       Ok(())
     })
+  // TODO: test for other linux distros
+    .on_page_load(|window, _payload| {
+      let zoom_level = if cfg!(target_os = "linux") {
+        1.4
+      } else {
+        1.0
+      };
+      if let Err(e) = window.set_zoom(zoom_level) {
+        eprintln!("Failed to set zoom: {}", e);
+      }
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
